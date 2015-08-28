fa8559a647be035d426f0fe7d03e0f3351ba121b
unknown <crystk@crystk-rtm.redmond.corp.microsoft.com>
2015-08-28 14:29:30 -0700

    Windows 10 does not support Hardware Token.  Using UUID instead

--- src/core/TelemetryContextWinStore.cpp
+++ src/core/TelemetryContextWinStore.cpp
@@ -81,22 +81,8 @@ void TelemetryContext::InitUser()
 /// Initializes the device.
 /// </summary>
 void TelemetryContext::InitDevice()
-{	
-	HardwareToken^ packageSpecificToken = HardwareIdentification::GetPackageSpecificToken(nullptr);
-	IBuffer^ hardwareId = packageSpecificToken->Id;
-	HashAlgorithmProvider^ algProv = HashAlgorithmProvider::OpenAlgorithm(HashAlgorithmNames::Sha256);
-	IBuffer^ hardwareIdHashed = algProv->HashData(hardwareId);
-	Array<unsigned char>^ buffer = ref new Array<unsigned char>(hardwareIdHashed->Length);
-	CryptographicBuffer::CopyToByteArray(hardwareIdHashed, &buffer);
-	std::wstring strDevId = L"";
-	
-	for (int i = 0; i < (int)hardwareIdHashed->Length; i++)
-	{
-		wchar_t hex[10];
-		swprintf_s(hex, 10, L"%02X", buffer[i]);
-		strDevId += hex;
-	}
-	Nullable<std::wstring> strId(strDevId);
+{
+	Nullable<std::wstring> strId(Utils::GenerateRandomUUID().c_str());
 	device.SetId(strId);
 
 	String^ lang = Language::CurrentInputMethodLanguageTag;
